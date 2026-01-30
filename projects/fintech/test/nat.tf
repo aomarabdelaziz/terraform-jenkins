5934a14f269226f0e1d62ad87bdd74ebd06b1c8e
aomarabdelaziz <abdelazizomar1337@gmail.com>
2026-01-30 08:45:06 +0200

    add new files

---
+++ projects/fintech/test/nat.tf
@@ -0,0 +1,19 @@
+resource "aws_eip" "eip" {
+  domain = "vpc"
+  tags = {
+    Name = "${local.env}-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_zone1.id
+
+
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "${local.env}-nat"
+  }
+
+
+}
